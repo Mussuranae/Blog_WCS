52ed894d381c7c85af585f3e0815e1d134b67864
Mussuranae <mandonnetdev@outlook.com>
2019-05-14 09:31:12 +0200

    test function sur slug

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -16,4 +16,14 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/index.html.twig', ['owner' => 'Thomas',]);
     }
+
+    /**
+     * @Route("/blog/show/{slug}", defaults={"slug"="Article Sans Titre"}, requirements={"slug"="[a-z0-9\-]*"}, name="blog_show")
+    */
+    public function show($slug)
+    {
+        $string = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('blog/show.html.twig', ['string' => $string]);
+    }
+
 }
