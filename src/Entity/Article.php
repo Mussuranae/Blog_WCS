96a5ed593b730a4204e192f3463959631c3ba638
Mussuranae <mandonnetdev@outlook.com>
2019-06-13 12:46:45 +0200

    validation

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,9 +6,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Service\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ * @UniqueEntity("title", message="Ce titre existe déjà")
  */
 class Article
 {
@@ -21,11 +24,15 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(pattern="/(digital)/", match=false, message="En français, il faut dire numérique")
      */
     private $content;
 
